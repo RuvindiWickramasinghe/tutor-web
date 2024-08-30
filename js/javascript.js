e381cd946f7c67a5be9a14162487260196b9d07c
Ruvindi Wickramasinghe <146427042+RuvindiWickramasinghe@users.noreply.github.com>
2024-08-30 19:42:12 +0530

    home page without sign in and sign up

---
+++ js/javascript.js
@@ -0,0 +1,17 @@
+document.querySelector('button').addEventListener('click', function() {
+    alert('Search functionality coming soon!');
+});
+$(document).ready(function() {
+    $('.pagination a').on('click', function(e) {
+      e.preventDefault();
+      var pageNumber = $(this).text();
+      // Call a function to load the next page of job listings
+      loadJobListings(pageNumber);
+    });
+  });
+  
+  function loadJobListings(pageNumber) {
+    // TO DO: implement the logic to load the next page of job listings
+    // For demonstration purposes, just console.log the page number
+    console.log('Loading page ' + pageNumber);
+  }
